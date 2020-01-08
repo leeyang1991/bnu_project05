b3867b353f1125f7c2396652de8ec1c09737334f
Yang Li <leeyang1991@gmail.com>
2020-01-08 13:15:46 +0800

    .

--- download_pdsi_terraclimate.py
+++ download_pdsi_terraclimate.py
@@ -13,12 +13,14 @@ for y in range(1982,2016):
     year.append(str(y))
 
 def download(y):
-    outdir = r'D:\project05\PDSI\download\\'
-    url = 'https://climate.northwestknowledge.net/TERRACLIMATE-DATA/TerraClimate_PDSI_{}.nc'.format(y)
+    outdir = r'D:\project05\PET\download\\'
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    url = 'https://climate.northwestknowledge.net/TERRACLIMATE-DATA/TerraClimate_pet_{}.nc'.format(y)
     video_i = requests.get(url)
     ts = video_i.content
-    movie = codecs.open(outdir+'PDSI_{}.nc'.format(y), 'wb')
+    movie = codecs.open(outdir+'pet_{}.nc'.format(y), 'wb')
     movie.write(ts)
 
 
-analysis.MUTIPROCESS(download,year).run(process=17,process_or_thread='t',text='downloading...')
+analysis.MUTIPROCESS(download,year).run(process=4,process_or_thread='t',text='downloading...')
