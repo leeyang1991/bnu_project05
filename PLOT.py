35b09d09bd71028df0e4b8e061ff6603eeb29196
Yang Li <leeyang1991@gmail.com>
2020-03-21 22:03:12 +0800

    plot_time_line

--- PLOT.py
+++ PLOT.py
@@ -3126,7 +3126,46 @@ def plot_bio_scatter():
         # plt.show()
 
 
+def plot_time_line():
+    # Y_dic = dict(np.load(Prepare1().this_class_arr + 'Y.npy').item())
+    #     #
+    #     # for key in Y_dic:
+    #     #     split_key = key.split('~')
+    #     #     pix, mark, eln, date_range = split_key
+    #     #     recovery = Y_dic[key]
+    #     #     print recovery
+    #     #     print split_key
+    # Create some mock data
+    t = np.arange(0.01, 10.0, 0.01)
+    data1 = np.exp(t)
+    data2 = np.sin(2 * np.pi * t)
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('exp', color=color)
+    ax1.plot(t, data1, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+    ax2.plot(t, data2, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()
+
+
+
+
+
     pass
+
+
+
 
 def main():
     # 1 plot events numbers
@@ -3145,7 +3184,7 @@ def main():
     # 4.2 time series
     # plot_time_series()
     # 5 Tenery plot
-    Ternary_plot().plot_scatter()
+    # Ternary_plot().plot_scatter()
     # 6 Threshold
     # Find_Threshold().run()
     # 7 Prepare Random Forest
@@ -3156,6 +3195,8 @@ def main():
     # Plot_RF_train_events_result().run()
     # 8 plot bio scatter
     # plot_bio_scatter()
+    # 9 plot time line
+    plot_time_line()
     pass
 
 
